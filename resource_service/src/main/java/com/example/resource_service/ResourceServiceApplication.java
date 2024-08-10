ac45ca1feba9a8d5b1231fb375b70e52a6913457
KamilWitczak242562 <242562@edu.p.lodz.pl>
2024-08-10 14:50:11 +0200

    fix

--- resource_service/src/main/java/com/example/resource_service/ResourceServiceApplication.java
+++ resource_service/src/main/java/com/example/resource_service/ResourceServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.resource_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class ResourceServiceApplication {
